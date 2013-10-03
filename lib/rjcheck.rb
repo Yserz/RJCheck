7b45076bd3ed94f5a025bc1e2b5bf06ca679bf8c
Yserz <michael.koppen@googlemail.com>
2013-10-03 12:03:43 +0200

    - some fixes

--- lib/rjcheck.rb
+++ lib/rjcheck.rb
@@ -2,13 +2,13 @@
 
 require 'bundler/setup'
 Bundler.require(:default)
-require_relative 'rubytest/meta_model/java/class'
-require_relative 'rubytest/meta_model/java/interface'
-require_relative 'rubytest/meta_model/java/method'
+require_relative 'rjcheck/meta_model/java/class'
+require_relative 'rjcheck/meta_model/java/interface'
+require_relative 'rjcheck/meta_model/java/method'
 
-require_relative 'rubytest/crawler/crawler'
-require_relative 'rubytest/crawler/java/java_crawler'
-require_relative 'rubytest/mapper/java/java_mapper'
+require_relative 'rjcheck/crawler/crawler'
+require_relative 'rjcheck/crawler/java/java_crawler'
+require_relative 'rjcheck/mapper/java/java_mapper'
 
 
 puts "Please enter the folder of the project: "
