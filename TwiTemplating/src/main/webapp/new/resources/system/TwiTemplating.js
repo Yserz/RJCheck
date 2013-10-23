46a337a649e4c5435fef83101e6715430f1131b8
Yserz <michael.koppen@googlemail.com>
2013-10-23 13:47:53 +0200

    cleanup

--- TwiTemplating/src/main/webapp/new/resources/system/TwiTemplating.js
+++ TwiTemplating/src/main/webapp/new/resources/system/TwiTemplating.js
@@ -4,56 +4,56 @@
 
 var TT = TT || {};
 (function() {
-	TT.debug = true;
+    TT.debug = true;
 
-	if (TT.debug) {
-		console.log("TT: ", "Initialize TwiTemplating!");
-	}
+    if (TT.debug) {
+        console.log("TT: ", "Initialize TwiTemplating!");
+    }
 
-	TT.loadedScripts;
+    TT.loadedScripts;
 
-	/*
-	 * This function loads js-files.
-	 * For this function jQuery is needed.
-	 */
-	TT.require = function(script) {
-		if (TT.loadedScripts === undefined) {
-			TT.loadedScripts = new Array();
-		}
-		// load scripts only once
-		if (TT.loadedScripts.indexOf(script) < 0) {
-			$.ajax({
-				url: script,
-				dataType: "script",
+    /*
+     * This function loads js-files.
+     * For this function jQuery is needed.
+     */
+    TT.require = function(script) {
+        if (TT.loadedScripts === undefined) {
+            TT.loadedScripts = new Array();
+        }
+        // load scripts only once
+        if (TT.loadedScripts.indexOf(script) < 0) {
+            $.ajax({
+                url: script,
+                dataType: "script",
 //			cache: true,
-				async: false, // scripts are neccessary *before* start executing them
-				success: function() {
-					if (TT.debug) {
-						console.log("TT: ", "Loaded Script: " + script);
-					}
-					TT.loadedScripts.push(script);
-				},
-				error: function() {
-					throw new Error("TT: Could not load script " + script);
-				}
-			});
-		}
-	};
+                async: false, // scripts are neccessary *before* start executing them
+                success: function() {
+                    if (TT.debug) {
+                        console.log("TT: ", "Loaded Script: " + script);
+                    }
+                    TT.loadedScripts.push(script);
+                },
+                error: function() {
+                    throw new Error("TT: Could not load script " + script);
+                }
+            });
+        }
+    };
 
-	/* import vendor scripts */
-	TT.require("resources/system/vendor/bootstrap.min.js");
-	TT.require("resources/system/vendor/highlight.js/highlight.pack.js");
-	/* import OwnBlog scripts */
-	TT.require("resources/system/TwiTemplatingWebservice.js");
-	TT.require("resources/system/TwiTemplatingRenderer.js");
+    /* import vendor scripts */
+    TT.require("resources/system/vendor/bootstrap.min.js");
+    TT.require("resources/system/vendor/highlight.js/highlight.pack.js");
+    /* import OwnBlog scripts */
+    TT.require("resources/system/TwiTemplatingWebservice.js");
+    TT.require("resources/system/TwiTemplatingRenderer.js");
 
-	// init Highlighting
-	hljs.initHighlightingOnLoad();
+    // init Highlighting
+    hljs.initHighlightingOnLoad();
 
-	if (TT.debug) {
-		// say hello to the world!
-		console.log("TT: ", "Welcome to your TwiTemplating!");
-	}
+    if (TT.debug) {
+        // say hello to the world!
+        console.log("TT: ", "Welcome to your TwiTemplating!");
+    }
 //	TT.Webservice.createBlog();
 //	console.log(TT.Webservice.getBlog(1));
 })();
--- TwiTemplating/src/main/webapp/new/resources/system/TwiTemplatingWebservice.js
+++ TwiTemplating/src/main/webapp/new/resources/system/TwiTemplatingWebservice.js
@@ -4,32 +4,32 @@
  */
 
 TT.Webservice = (function() {
-	var Webservice = {};
+    var Webservice = {};
 
-	Webservice.TT_WEBSERVICE_ADDRESS = "http://localhost:8080/";
+    Webservice.TT_WEBSERVICE_ADDRESS = "http://localhost:8080/";
 
 
-	Webservice.ping = function() {
-		$.ajax({
-			type: "GET",
-			dataType: "text",
-			url: Webservice.TT_WEBSERVICE_ADDRESS + "TwiTemplating/rest/v1/service/ping",
-			async: true,
-			complete: function(jqXHR, textStatus) {
-				switch (jqXHR.status) {
-					case 404:
-						console.log(jqXHR);
-						throw new Error("Resource Not Found: " + jqXHR.responseText);
-					case 500:
-						console.log(jqXHR);
-						throw new Error("Internal Server Error: " + jqXHR.responseJSON.errorMsg);
-					default:
-						console.log(jqXHR.responseText);
-				}
-			}
-		});
+    Webservice.ping = function() {
+        $.ajax({
+            type: "GET",
+            dataType: "text",
+            url: Webservice.TT_WEBSERVICE_ADDRESS + "TwiTemplating/rest/v1/service/ping",
+            async: true,
+            complete: function(jqXHR, textStatus) {
+                switch (jqXHR.status) {
+                    case 404:
+                        console.log(jqXHR);
+                        throw new Error("Resource Not Found: " + jqXHR.responseText);
+                    case 500:
+                        console.log(jqXHR);
+                        throw new Error("Internal Server Error: " + jqXHR.responseJSON.errorMsg);
+                    default:
+                        console.log(jqXHR.responseText);
+                }
+            }
+        });
 
-	};
+    };
 
-	return Webservice;
+    return Webservice;
 }());
