9d9da2da56e484d1b6edf1ed6266c0da50113b19
Dragon <Dragon@Dragon-LAP>
2013-10-30 21:40:27 +0100

    adding some regex

--- lib/rjcheck/mapper/java/java_mapper.rb
+++ lib/rjcheck/mapper/java/java_mapper.rb
@@ -1,6 +1,8 @@
 class JavaMapper
         # matches \*package\*
-        #        Package_signature = ".*package.*"
+        Package_signature = "package\s([a-zA-Z.]+);"
+        # matches import
+        Import_signature = "import\s([a-zA-Z.]+);"
         # matches <Object>
         Generic_signature = "(?:<[A-Z]{1}[a-zA-Z0-9]*>)?"
         # matches _static
@@ -30,6 +32,37 @@ class JavaMapper
   end
 
         def map_file(path, text)
+          
+                class_regex = Regexp.new(Package_signature,Regexp::MULTILINE )
+                if text.match(class_regex)
+                        puts "Matches Class"
+                        puts "#{path} "#==> \n#{text.match(regex)}"
+
+                        match = class_regex.match(text)
+                        if match
+                                puts "Package: #{match[1]}"
+                        end
+                end
+                
+                class_regex = Regexp.new(Import_signature,Regexp::MULTILINE)
+                if text.match(class_regex)
+                        puts "Matches Import"
+                        puts "#{path} "#==> \n#{text.match(regex)}"
+
+                        #match = class_regex.match(text)
+                        match = text.scan(class_regex)
+                        if match
+                                match.each do |i|
+                                  #test = match[i]
+                                  i.each do |j|
+                                    puts "Import: #{j}"
+                                  end
+                                end
+                        end
+                end
+          
+          
+          
                 class_regex = Regexp.new(Class_signature,Regexp::MULTILINE )
 
 
@@ -44,11 +77,12 @@ class JavaMapper
                                 puts "Class-Visability: #{match[2]}"
                                 puts "Class-Name: #{match[3]}"
                                 puts "Class-Extends: #{match[4]}"
-                                puts "Class-Extends: #{match[5]}"
+                                #puts "Class-Extends: #{match[5]}"
+                                puts "Class-Implements: #{match[5]}"
                                 puts "Class-Implements: #{match[6]}"
-                                puts "Class-Implements: #{match[7]}"
                                 #puts "Class-Body: #{match[8]}"
                         end
+           
                         
                         if /\sfinal\s/.match(text)
                                 puts "Class is Final"
@@ -60,6 +94,12 @@ class JavaMapper
                                 puts "Class is Static"
                         else
                                 puts "Class is Not Static"
+                        end
+                        
+                        if /\sabstract\s/.match(text)
+                                puts "Class is Abstract"
+                        else
+                                puts "Class is Not Abstract"
                         end
               
               
