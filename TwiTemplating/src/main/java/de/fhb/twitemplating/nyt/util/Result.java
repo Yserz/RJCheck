b32f65f5acbc03f77973bd572f62e8dc6174f1c1
Yserz <michael.koppen@googlemail.com>
2013-10-31 19:27:04 +0100

    -

--- TwiTemplating/src/main/java/de/fhb/twitemplating/nyt/util/Result.java
+++ TwiTemplating/src/main/java/de/fhb/twitemplating/nyt/util/Result.java
@@ -5,6 +5,7 @@ import com.google.gson.annotations.*;
 import java.util.*;
 
 public class Result {
+
 	public String section;
 	public String title;
 	public String published_date;
@@ -12,12 +13,12 @@ public class Result {
 	@SerializedName("abstract")
 	public String isAbstract;
 	public List<Media> media;
-    public String url;
+	public String url;
 
-	public String getSmallImageURL(){
+	public String getSmallImageURL() {
 		if (media != null && !media.isEmpty()) {
-			for (Media m : media){
-				if (m.metaData.get(0).height < 100){
+			for (Media m : media) {
+				if (m.metaData.get(0).height > 400) {
 					return m.metaData.get(0).url;
 				}
 			}
