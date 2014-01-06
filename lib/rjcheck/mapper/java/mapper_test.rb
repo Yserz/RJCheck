5d891eb5384cb8e8a4a28f778d153bb8c51c391d
Salaxy <salaxy@web.de>
2014-01-06 20:29:19 +0100

    create mapper_test

---
+++ lib/rjcheck/mapper/java/mapper_test.rb
@@ -0,0 +1,39 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+#
+#
+#
+#
+
+class MainController
+	attr_accessor :folder
+	attr_accessor :crawler
+	attr_accessor :mapper
+  def initialize(folder)
+		@folder = folder
+		@crawler = JavaCrawler.new(@folder)
+		@mapper = JavaMapper.new()
+  end
+
+	def run
+
+		#puts "\n\n\n *** Crawl everything ***"
+		#crawler = Crawler.new(folder)
+		#crawler.crawl {|file| puts "InData: #{file}"}
+
+		file_list = crawl
+		file_list.each { |key,value|  puts "\n\nLOG: key: #{key}\nvalue: #{value}"}
+		map(file_list)
+	end
+
+	def crawl
+		puts "\n\n\n *** Crawl Java ***"
+		file_list = Hash.new()
+		@crawler.crawl {|path,file| file_list[path]=file}
+		file_list
+	end
+
+	def map(file_list)
+		@mapper.map(file_list)
+	end
+end
