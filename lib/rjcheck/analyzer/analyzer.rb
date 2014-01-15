1f8a7939f07a8382706cb8087741d6b4d3a750c7
Salaxy <salaxy@web.de>
2014-01-15 17:03:19 +0100

    begin implement  analyzer

--- lib/rjcheck/analyzer/analyzer.rb
+++ lib/rjcheck/analyzer/analyzer.rb
@@ -9,19 +9,42 @@
 
 class Analyzer
 	
-		attr_accessor :java_map
+	attr_accessor :java_map
+	attr_accessor :dsl_object
 	
   def initialize
     
   end
 	
-  def analyse(java_map)
+  def analyse(java_map, dsl_object)
     @java_map=java_map
+		@dsl_object=dsl_object
 		
-		
-		
-		
-		
+		# alle java klassen durch laufen
+		# key = package + name, value = java_file
+		file_list.each { |key,value|  analyse_file(key,value)}
   end	
+	
+	def analyse_file(key,value)
+
+		is_entity = false
+		is_repository = false
+		is_manager = false
+		
+		entities_package = @dsl_object.entities_package
+		repositories_package = @dsl_object.repositories_package
+		manager_package = @dsl_object.manager_package
+	
+		if key.include? entities_package
+
+		elsif key.include? repositories_package
+   
+		elsif key.include? manager_package
+   
+		else
+			
+		end
+	
+	end
 	
 end
