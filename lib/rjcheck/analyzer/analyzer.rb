93eb9e57e611269fdcf0dee97e2718f09968c3a9
Salaxy <salaxy@web.de>
2014-01-15 15:40:48 +0100

    dummy analyser created

---
+++ lib/rjcheck/analyzer/analyzer.rb
@@ -0,0 +1,27 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+#
+#	author: Andy Klay <klay@fh-brandenburg.de>
+#
+#
+# This class analyse the layer seperation of a mapped the project
+#
+
+class Analyzer
+	
+		attr_accessor :java_map
+	
+  def initialize
+    
+  end
+	
+  def analyse(java_map)
+    @java_map=java_map
+		
+		
+		
+		
+		
+  end	
+	
+end
