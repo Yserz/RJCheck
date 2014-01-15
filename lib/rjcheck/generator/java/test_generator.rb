e62969744a61aaa4c8993fc5089ba3dc6607a33d
kaain <ott@fh-brandenburg.de>
2014-01-15 17:59:09 +0100

    - generating test classes

--- lib/rjcheck/generator/java/test_generator.rb
+++ lib/rjcheck/generator/java/test_generator.rb
@@ -25,13 +25,18 @@ class TestGenerator
 		puts '####### generate Test for: ' +class_object.identifier
 
 		path_to_class = class_object.package.gsub('.','/')
-		puts OS.replace_separator path_to_class
+		path_to_class = OS.replace_separator path_to_class
 
 		puts "#{@dsl.main_test_folder_path}#{path_to_class}"
 
-		#File.open(@dsl.main_test_folder_path +path_to_class, 'w') do |file|
-		#	# use "\n" for two lines of text
-		#	file.puts "Test"
-		#end
+		path_to_class = "#{@dsl.main_test_folder_path}#{path_to_class}"
+		FileUtils.mkdir_p path_to_class
+		path_to_class = "#{path_to_class}/#{class_object.identifier}.java"
+		path_to_class = OS.replace_separator path_to_class
+		puts "write to: #{path_to_class}"
+		File.open(path_to_class, 'w+') do |file|
+			# use "\n" for two lines of text
+			file.write class_object.package
+		end
 	end
 end
