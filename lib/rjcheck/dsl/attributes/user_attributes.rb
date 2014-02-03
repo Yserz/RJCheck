42809f2abb035aa549ef90f223a9821a5aefd6d5
Yserz <michael.koppen@googlemail.com>
2014-02-03 17:44:13 +0100

    - doku recipe - cleanup

--- lib/rjcheck/dsl/attributes/user_attributes.rb
+++ lib/rjcheck/dsl/attributes/user_attributes.rb
@@ -13,22 +13,10 @@ NODE['main_test_folder']['test_resources_folder'] = Hash.new
 # Paths have to be expresses with '/'!
 # Please note the '/' on the end of the path.
 # For windows use e.g.: 'C:/Users/User/Desktop/Project/'
-#NODE['path']																											= "C:/Users/Salaxy/Desktop/RJCheckExample/"
 NODE['path']																											= "/Users/MacYser/Documents/Workspaces/NetBeansWorkspace/RJCheckExample/"
-#NODE['path']																											= 'path/to/project_folder'
-#NODE['main_source_folder']['path']																= NODE['path'].to_s + 'src/main/'
-#NODE['main_source_folder']['source_folder']['path']								= NODE['main_source_folder']['path'].to_s + 'java/'
-#NODE['main_source_folder']['source_folder']['type']								= 'java'
-#NODE['main_source_folder']['source_resources_folder']['path']			= NODE['main_source_folder']['path'].to_s + 'resources/'
-#
-#NODE['main_test_folder']['path']																	= NODE['path'] + 'src/test/'
-#NODE['main_test_folder']['test_folder']['path']										= NODE['main_test_folder']['path'].to_s + 'java/'
-#NODE['main_test_folder']['test_folder']['type']										= 'java'
-#NODE['main_test_folder']['test_resources_folder']['path']					= NODE['main_test_folder']['path'].to_s + 'resources/'
 
+NODE['entities_package']																					= 'de.fhb.rjcheckexample.entity'
+NODE['repositories_package']																			= 'de.fhb.rjcheckexample.repositoryfail'
+NODE['manager_package']																						= 'de.fhb.rjcheckexample.manager'
 
-NODE['entities_package']										= 'de.fhb.rjcheckexample.entity'
-NODE['repositories_package']									= 'de.fhb.rjcheckexample.repository'
-NODE['manager_package']											= 'de.fhb.rjcheckexample.manager'
-
-NODE['generate_test_classes']									= true
+NODE['generate_test_classes']																			= true
--- lib/rjcheck/dsl/controller/recipe.rb
+++ lib/rjcheck/dsl/controller/recipe.rb
@@ -1,7 +1,10 @@
+# This is the recipe-file. Its a DSL which describes the structure of an EJB-project.
+
 require 'rjcheck/dsl/attributes/user_attributes.rb'
 
 $RJ = DSLController.new()
 
+# This is a method call of the method define_sources. It sends a ruby block as parameter which will overwrite the default-attributes.
 $RJ.define_sources NODE['path'] do
 	main_source_folder_path				NODE['main_source_folder']['path']
 	source_folder_path						NODE['main_source_folder']['source_folder']['path']
@@ -9,6 +12,7 @@ $RJ.define_sources NODE['path'] do
 	source_resources_folder_path	NODE['main_source_folder']['source_resources_folder']['path']
 end
 
+# This is a method call of the method define_tests. It sends a ruby block as parameter which will overwrite the default-attributes.
 $RJ.define_tests NODE['path'] do
 	generate_test_classes					NODE['generate_test_classes']
 	main_test_folder_path					NODE['main_test_folder']['path']
@@ -17,6 +21,7 @@ $RJ.define_tests NODE['path'] do
 	test_resources_folder_path		NODE['main_test_folder']['test_resources_folder']['path']
 end
 
+# This is a method call of the method define_layers. It sends a ruby block as parameter which will overwrite the default-attributes.
 $RJ.define_layers do
 	entities_package							NODE['entities_package']
 	repositories_package					NODE['repositories_package']
